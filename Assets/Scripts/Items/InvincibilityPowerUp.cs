92fdcb5eb01f5a246cfa7be69839289986a9bebe
Daniel Martí <49535803+damacaa@users.noreply.github.com>
2021-12-31 16:09:35 +0100

    Merge

--- Assets/Scripts/Items/InvincibilityPowerUp.cs
+++ Assets/Scripts/Items/InvincibilityPowerUp.cs
@@ -6,11 +6,11 @@ public class InvincibilityPowerUp : PowerUp
 {
     protected override void Effect()
     {
-
+        AudioManager.instance.PlayOnce("recogerJeringuilla");
         if (!PlayerController.instance.injured)
             return;
 
-        AudioManager.instance.PlayOnce("recogerCocacola");
+        
         PlayerController.instance.Heal();
         Destroy(gameObject);
     }
