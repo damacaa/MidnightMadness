d1e7d4a6df748f90328df138fda96d746cf814eb
M-Galvez <m.galvez.2018@alumnos.urjc.es>
2021-12-31 15:48:57 +0100

    Power up effects

--- Assets/Scripts/Items/SpeedPowerUp.cs
+++ Assets/Scripts/Items/SpeedPowerUp.cs
@@ -6,11 +6,11 @@ public class SpeedPowerUp : PowerUp
 {
     protected override void Effect()
     {
-
+        AudioManager.instance.PlayOnce("recogerPastillas");
         if (!PlayerController.instance.injured)
             return;
 
-        AudioManager.instance.PlayOnce("recogerCocacola");
+        
         PlayerController.instance.Heal();
         Destroy(gameObject);
     }
