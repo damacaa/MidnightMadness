b6efb537af4160e538d9743775fddef5be0505e1
M-Galvez <m.galvez.2018@alumnos.urjc.es>
2021-12-31 08:57:49 +0100

    Sound effects

--- Assets/Scripts/Items/HealthPowerUp.cs
+++ Assets/Scripts/Items/HealthPowerUp.cs
@@ -6,9 +6,11 @@ public class HealthPowerUp : PowerUp
 {
     protected override void Effect()
     {
+        
         if (!PlayerController.instance.injured)
             return;
 
+        AudioManager.instance.PlayOnce("recogerCocacola");
         PlayerController.instance.Heal();
         Destroy(gameObject);
     }
